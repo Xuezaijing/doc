99f500072209c618591d5b7c74fcfd7822e8ad90
xuezaijing <1062147007@qq.com>
2019-06-17 17:50:33 +0800

    mybatis generate

---
+++ mall/mall-mbg/src/main/java/com/xzj/mall/mapper/CmsMemberReportMapper.java
@@ -0,0 +1,22 @@
+package com.xzj.mall.mapper;
+
+import com.xzj.mall.model.CmsMemberReport;
+import com.xzj.mall.model.CmsMemberReportExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface CmsMemberReportMapper {
+    int countByExample(CmsMemberReportExample example);
+
+    int deleteByExample(CmsMemberReportExample example);
+
+    int insert(CmsMemberReport record);
+
+    int insertSelective(CmsMemberReport record);
+
+    List<CmsMemberReport> selectByExample(CmsMemberReportExample example);
+
+    int updateByExampleSelective(@Param("record") CmsMemberReport record, @Param("example") CmsMemberReportExample example);
+
+    int updateByExample(@Param("record") CmsMemberReport record, @Param("example") CmsMemberReportExample example);
+}
